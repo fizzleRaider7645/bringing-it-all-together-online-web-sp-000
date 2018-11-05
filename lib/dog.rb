50c5cf260fdd3a11e8e0594d77dbdf9111aef9b2
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 20:55:33 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,82 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+  end
+  
+  def self.new_from_db(row)
+    Dog.new(name: row[1], breed: row[2], id: row[0])
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name)
+    self.new_from_db(row)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, name, breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten[0]
+    self
+  end
+  
+  def self.create(attributes)
+    name = attributes[:name]
+    breed = attributes[:breed]
+    new_dog = Dog.new(name: name, breed: breed)
+    new_dog.save
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+    row = DB[:conn].execute(sql, id).flatten
+    self.new_from_db(row)
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
+    if !dog.empty?
+      Dog.new(dog[1], dog[2], dog[0])
+    else
+      dog = Dog.new(name: name, breed: breed)
+      dog.save
+    end
+    dog
+  end
+end
