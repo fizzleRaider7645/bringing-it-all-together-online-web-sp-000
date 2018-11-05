b090000b0a6f713a3904df34c66d2faa04e47468
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 21:07:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ class Dog
     if !dog_row.empty?
       dog = Dog.create(name: dog_row[1], breed: dog_row[2], id: dog_row[3])
     else
-      dog = Dog.create(name: name, breed: breed)
+      self.new_from_db(row)
     end
     dog
   end
